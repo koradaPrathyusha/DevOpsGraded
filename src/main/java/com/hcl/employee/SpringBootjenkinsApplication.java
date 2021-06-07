81fa41b5ebb1aed94dd2f6116f23aab95340e28c
koradaPrathyusha <prathyushasrinu1826@gmail.com>
2021-06-07 11:30:56 +0530

    firstjenkin

---
+++ src/main/java/com/hcl/employee/SpringBootjenkinsApplication.java
@@ -0,0 +1,14 @@
+package com.hcl.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootjenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootjenkinsApplication.class, args);
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
---
+++ src/test/java/com/hcl/employee/SpringBootjenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootjenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
